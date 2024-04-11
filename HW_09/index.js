d1ffa806c2fd78d2d961aea2074ea9cfbc2f21bf
Ivan <mishchenkoivan36@gmail.com>
2024-04-11 16:38:04 +0200

    HW_09

---
+++ HW_09/index.js
@@ -0,0 +1,24 @@
+// найдем первую кнопку по id
+const btn = document.getElementById("magic-btn");
+// скопируем ее - получилась вторая кнопка. Ее можно использовать.
+const clonedBtn = btn.cloneNode(true);
+// задали id
+clonedBtn.id = "magic-btn-2";
+
+btn.addEventListener('click', () => {
+  // при нажатии на первую кнопку
+  // работайте с clonedBtn
+  // ваш код начинается здесь
+    document.body.appendChild(clonedBtn);
+  });
+
+// меняю текст на новой кнопке и крашу ее  коричневый цвет
+clonedBtn.textContent = "Я изменю тебя";
+clonedBtn.style.backgroundColor = "#a78b71";
+clonedBtn.style.color = "white";
+//при клике на новую кнопку первая меняет свой цвет
+clonedBtn.addEventListener('click', () => {
+  
+  btn.style.backgroundColor = "#9c4a1a"; 
+  btn.style.color = "black"; 
+});
